b1ed27c8b338c8c3faf4a0d07fad7dcc6ba97a91
Tegan Broderick <teganbroderick@gmail.com>
2019-12-13 20:59:27 +0000

    Testing with DarkSky python library for making Darksky weather api calls

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+from darksky.api import DarkSky, DarkSkyAsync
+from darksky.types import languages, units, weather
 
 app = Flask(__name__)
 
@@ -12,10 +14,35 @@ app.secret_key = "ABC"
 # error.
 app.jinja_env.undefined = StrictUndefined
 
+darksky = DarkSky()
+
 
 @app.route('/')
 def index():
     """Render index.html"""
+    latitude = 37.877360
+    longitude = -122.296730
+    forecast = darksky.get_forecast(
+        latitude, longitude,
+        extend=False, # default `False`
+        lang=languages.ENGLISH, # default `ENGLISH`
+        units=units.AUTO, # default `auto`
+        exclude=[weather.MINUTELY, weather.ALERTS], # default `[]`,
+        timezone='UTC' # default None - will be set by DarkSky API automatically
+        )   
+    forecast.latitude # 42.3601
+    forecast.longitude # -71.0589
+    forecast.timezone # timezone for coordinates. For exmaple: `America/New_York`
+
+    forecast.currently # CurrentlyForecast. Can be found at darksky/forecast.py
+    forecast.minutely # MinutelyForecast. Can be found at darksky/forecast.py
+    forecast.hourly # HourlyForecast. Can be found at darksky/forecast.py
+    forecast.daily # DailyForecast. Can be found at darksky/forecast.py
+    forecast.alerts # [Alert]. Can be found at darksky/forecast.py
+
+    
+    print(forecast.daily)
+
     return render_template("index.html")
 
 
