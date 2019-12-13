56c8f671741108ff762eb6370f81840d5ac23ced
Tegan Broderick <teganbroderick@gmail.com>
2019-12-13 21:18:26 +0000

    Refactored code to use darkskylib

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-from darksky.api import DarkSky, DarkSkyAsync
-from darksky.types import languages, units, weather
+from darksky import forecast
+from datetime import date, timedelta
 
 app = Flask(__name__)
 
@@ -14,34 +14,26 @@ app.secret_key = "ABC"
 # error.
 app.jinja_env.undefined = StrictUndefined
 
-darksky = DarkSky()
+
 
 
 @app.route('/')
 def index():
     """Render index.html"""
-    latitude = 37.877360
-    longitude = -122.296730
-    forecast = darksky.get_forecast(
-        latitude, longitude,
-        extend=False, # default `False`
-        lang=languages.ENGLISH, # default `ENGLISH`
-        units=units.AUTO, # default `auto`
-        exclude=[weather.MINUTELY, weather.ALERTS], # default `[]`,
-        timezone='UTC' # default None - will be set by DarkSky API automatically
-        )   
-    forecast.latitude # 42.3601
-    forecast.longitude # -71.0589
-    forecast.timezone # timezone for coordinates. For exmaple: `America/New_York`
-
-    forecast.currently # CurrentlyForecast. Can be found at darksky/forecast.py
-    forecast.minutely # MinutelyForecast. Can be found at darksky/forecast.py
-    forecast.hourly # HourlyForecast. Can be found at darksky/forecast.py
-    forecast.daily # DailyForecast. Can be found at darksky/forecast.py
-    forecast.alerts # [Alert]. Can be found at darksky/forecast.py
-
     
-    print(forecast.daily)
+    BERKELEY = 37.877360, -122.296730
+
+    weekday = date.today()
+    with forecast("API_KEY", *BERKELEY) as berkeley:
+        print(berkeley.daily.summary, end='\n---\n')
+    for day in berkeley.daily:
+        day = dict(day = date.strftime(weekday, '%a'),
+                   sum = day.summary,
+                   tempMin = day.temperatureMin,
+                   tempMax = day.temperatureMax
+                   )
+        print('{day}: {sum} Temp range: {tempMin} - {tempMax}'.format(**day))
+        weekday += timedelta(days=1)
 
     return render_template("index.html")
 
