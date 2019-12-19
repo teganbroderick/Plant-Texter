5e5d18abece0c54b3dd42db8a75f2e82f308d605
Tegan Broderick <teganbroderick@gmail.com>
2019-12-19 20:43:17 +0000

    Added while loop that calls check_weather function every morning at 9am. Tested code, sms is sent successfully if max_temp >= 25.

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,6 @@
 from twilio.rest import Client
 import schedule
+import time
 from darksky import forecast
 from datetime import date, timedelta
 import emoji
@@ -18,22 +19,29 @@ from_number = os.environ['from_number']
 
 client = Client(account_sid, auth_token)
 
-def send_sms():
-    """Sends sms message if today's maximum temperature is going to be >= 25 degrees celcius"""
+def check_weather():
+    """Get today's max temperature in berkeley.
+    Send sms message if max temperature is above 25 degrees celcius
+    """
+    
     BERKELEY = 37.877360, -122.296730
 
     with forecast(dark_sky, *BERKELEY) as berkeley: 
         todays_max_temp_in_fahrenheit = (berkeley.daily[0]).temperatureMax 
         max_temp_in_celcius = helpers.convert_fahrenheit_to_celcius(todays_max_temp_in_fahrenheit)   
     
-    if max_temp_in_celcius <= 25:
+    #if max_temp above 25, send sms
+    if max_temp_in_celcius >= 25:
         message = client.messages.create(
             from_=from_number, 
             to=my_number,
             body=emoji.emojize("It is unseasonably warm today. You should consider giving your plants some extra water. :seedling:"))
-
         print(message.sid)      
 
-send_sms()
+# check_weather()
 
-# schedule.every().day.at('09:00').do(send_sms)
+schedule.every().day.at('09:00').do(check_weather)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
