de08777f4b375d362d292235361955efb7b71769
Tegan Broderick <teganbroderick@gmail.com>
2019-12-19 04:09:06 +0000

    Added function that gets today's max temp, and sends an sms through twilio if the max temp is less than 25 degrees (for testing purposes). Function works, texting works.

--- scheduler.py
+++ scheduler.py
@@ -1,19 +1,38 @@
-from flask import Flask
-from server import *
-
-import schedule
-import requests, json, time
-
-from datetime import date
-
 from twilio.rest import Client
+import schedule
+from darksky import forecast
+from datetime import date, timedelta
 
 import helpers
 import os
 
-app = Flask(__name__)
+#Get darksky api key
+dark_sky = os.environ['DARK_SKY_API']
 
-def job():
-    """Finds out whether today's temperature is above 
-    25 degrees celcius, sends text if it is.
-    """
+#Get twilio api keys
+account_sid = os.environ['account_sid']
+auth_token  = os.environ['auth_token']
+my_number = os.environ['my_number']
+from_number = os.environ['from_number']
+
+client = Client(account_sid, auth_token)
+
+def send_sms():
+    """Sends sms message if today's maximum temperature is going to be >= 25 degrees celcius"""
+    BERKELEY = 37.877360, -122.296730
+
+    with forecast(dark_sky, *BERKELEY) as berkeley: 
+        todays_max_temp_in_fahrenheit = (berkeley.daily[0]).temperatureMax 
+        max_temp_in_celcius = helpers.convert_fahrenheit_to_celcius(todays_max_temp_in_fahrenheit)   
+    
+    if max_temp_in_celcius <= 25:
+        message = client.messages.create(
+            from_=from_number, 
+            to=my_number,
+            body="It is unseasonably warm today. You should consider giving your plants some extra water.")
+
+        print(message.sid)      
+
+send_sms()
+
+# schedule.every().day.at('09:00').do(send_sms)
